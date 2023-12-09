d2dcba5329896b4507a6c9ea48c9302107f3e86a
Nikolas-Pensyl <45053327+Nikolas-Pensyl@users.noreply.github.com>
2023-12-09 00:30:31 -0500

    Create part1.py

---
+++ 2023/day9/part1.py
@@ -0,0 +1,23 @@
+file = open("text.txt")
+content = [[list(list(map(int, l.split())))] for l in file.readlines()]
+
+nextVals = []
+total = 0
+
+
+for i in range(len(content)):
+
+    #Get All difs to 0
+    while not all([ x==0 for x in content[i][-1]]):
+        content[i].append([content[i][-1][j+1] - content[i][-1][j] for j in range(len(content[i][-1])-1)])
+
+
+    nextVal = 0
+    for j in range(len(content[i])-1, -1, -1):
+        nextVal += content[i][j][-1]
+
+    nextVals.append(nextVal)
+    total += nextVal
+
+
+print(total)
