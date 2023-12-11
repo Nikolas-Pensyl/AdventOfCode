705ced10dd66ace23006994d6dce100aed294ed9
Nikolas-Pensyl <45053327+Nikolas-Pensyl@users.noreply.github.com>
2023-12-11 08:57:34 -0500

    Create part1.py

---
+++ 2023/day11/part1.py
@@ -0,0 +1,49 @@
+file = open("text.txt")
+content = file.readlines()
+content = [[content[i][j] for j in range(len(content[i])) if content[i][j] != '\n'] for i in range(len(content))]
+
+nodeNumber = 1
+i=0
+while i<len(content):
+    isEmpty = True
+    for j in range(len(content[i])):
+        if content[i][j] == '#':
+            isEmpty = False
+            break
+    if isEmpty:
+        print(i)
+        content.insert(i, content[i])
+        i +=1
+    i+=1
+
+j=0
+while j<len(content[0]):
+    isEmpty = True
+    for i in range(len(content)):
+        if content[i][j] == '#':
+            isEmpty = False
+            break
+    if isEmpty:
+        for i in range(len(content)):
+            content[i].insert(j, '.')
+        j +=1
+    j +=1
+
+nodeNumber = 1
+NodeLocs = []
+for i in range(len(content)):
+    for j in range(len(content[i])):
+        if content[i][j] == '#':
+            content[i][j] = nodeNumber
+            NodeLocs.append([j, i])
+            nodeNumber +=1
+        else:
+            content[i][j] = 0
+
+
+total = 0
+for i in range(len(NodeLocs)-1):
+    for j in range(i+1, len(NodeLocs)):
+        total += abs(NodeLocs[i][1]-NodeLocs[j][1]) + abs(NodeLocs[i][0]-NodeLocs[j][0])
+
+print(total)
